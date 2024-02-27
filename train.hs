e0e35f7edd30dad6a140185c51c2dfbe1d9a1bef
kosmong <kosmo.ng@gmail.com>
2024-02-26 19:12:57 -0800

    fill emission matrix sort of completed, needs debugging

--- train.hs
+++ train.hs
@@ -2,10 +2,13 @@ data HMMMatrix = Empty
         | Matrix [String] [String] [[Double]]
     deriving Show
 
+data HMMModel = None
+        | Model HMMMatrix HMMMatrix
+
 -- get data from the matrix
 getData :: HMMMatrix -> String -> String -> Maybe Double
 getData Empty _ _ = Nothing
-getData (Matrix lst1 lst2 lstoflst) str1 str2 = 
+getData (Matrix lst1 lst2 lstoflst) str1 str2 =
     if (str1 `elem` lst1 && str2 `elem` lst2) then
         Just ((lstoflst !! (find lst1 str1 0)) !! find lst2 str2 0)
     else
@@ -22,14 +25,14 @@ changeElem 0 col x (l:ls) = replaceAtIndex col x l : ls  -- Reduce the column
 changeElem row col x (l:ls) = l : changeElem (row - 1) col x ls  -- Reduce the row
 
 changeElem_str :: String -> String -> Double -> HMMMatrix -> HMMMatrix
-changeElem_str str1 str2 x (Matrix lst1 lst2 lstoflst) = 
+changeElem_str str1 str2 x (Matrix lst1 lst2 lstoflst) =
     Matrix lst1 lst2 (changeElem (find lst1 str1 0) (find lst2 str2 0) x lstoflst)
-    
+
 
 -- find the element return index if found, return -1 if not found
 find :: (Eq t) => [t] -> t -> Int -> Int
 find [] _ _ = -1
-find (h : t) s n 
+find (h : t) s n
     | h == s = n
     | otherwise = find t s (n+1)
 
@@ -50,9 +53,9 @@ helperfun (h:t) acc
 
 -- Loads words from a text file into a list.
 readtxt :: FilePath -> IO String
-readtxt path = do 
+readtxt path = do
     contents <- readFile path
-    let 
+    let
         -- list of pairs: first is a word, second is the pos
         pair_lst = (map split (lines contents))
 
@@ -67,13 +70,13 @@ readtxt path = do
         -- matrix initialization
         transition_matrix = initMatrix ("<S>": pos_lst) (pos_lst ++ ["<E>"])
         emission_matrix = initMatrix word_lst pos_lst
-    
+
     return contents
 
-makeMatrixes :: FilePath -> IO (HMMMatrix, HMMMatrix)
+makeMatrixes :: FilePath -> IO HMMModel
 makeMatrixes path = do
     contents <- readFile path
-    let 
+    let
         sentences = lines contents
         -- list of pairs: first is a word, second is the pos
         pair_lst = map split sentences
@@ -83,18 +86,21 @@ makeMatrixes path = do
         pos = map snd pair_lst
 
         -- get word and pos counts as well
-        word_counts = newElemCount words [] []
-        pos_counts = newElemCount pos [] []
+        wrd_c = newElemCount words [] []
+        pos_c = newElemCount pos [] []
+
+        word_counts = uncurry zip wrd_c
+        pos_counts = uncurry zip pos_c
 
         -- words and pos lst
-        word_lst = fst word_counts
-        pos_lst = fst pos_counts
+        word_lst = fst wrd_c
+        pos_lst = fst pos_c
 
         -- matrix initialization
         transition_matrix = initMatrix ("<S>": pos_lst) (pos_lst ++ ["<E>"])
         emission_matrix = initMatrix word_lst pos_lst
-    
-    return (transition_matrix, emission_matrix)
+
+    return (Model transition_matrix emission_matrix)
 
 -- filter out redundant elements and count number of ocurrences
 newElemCount :: Eq a => [a] -> [a] -> [Double] -> ([a],[Double])
@@ -105,7 +111,7 @@ newElemCount (h:t) acc count
 
 -- add to counts at specified index
 addIndex :: [Double] -> Int -> Int ->  [Double]
-addIndex (h:t) index acc 
+addIndex (h:t) index acc
     | index == acc = h+1:t
     | otherwise = h: addIndex t index (acc+1)
 
@@ -117,29 +123,23 @@ newElem (h:t) acc
     | otherwise = newElem t (h : acc)
 
 
-fillEmission :: HMMMatrix -> [(String,Double)] -> [(String,String)] -> HMMMatrix
-fillEmission (Matrix wrds pos probs) pos_counts pairs =
-    -- for each category and word:
-    -- get pairs with certain category from list of pairs
-    -- count co-occurences of category and word
-    -- get total occurences of category
-    -- use the 2 counts to calculate emission probability of the co-ocurrence of a category and word
-    --still need to map
-    let 
-        wrd = fst (head pairs)
-        cat = snd (head pairs)
-        prob = calculateEmissions wrd cat pos_counts pairs
-        -- modProbs = modifyMatrix probs 0 0 prob
+fillEmissionMatrix :: HMMMatrix -> [(String,Double)] -> [(String,String)]-> [(String,String)] -> HMMMatrix
+fillEmissionMatrix Empty _ _ _ = Empty
+fillEmissionMatrix e_matrix _ _ [] = e_matrix
+fillEmissionMatrix (Matrix lst1 lst2 lstoflst) pos_counts pairs (p1:rest) = 
+    let
+        curr_emission_matrix = calculateEmission (Matrix lst1 lst2 lstoflst) (fst p1) (snd p1) pos_counts pairs
     in
-    -- (Matrix wrds pos modProbs)
-    changeElem_str wrd cat prob (Matrix wrds pos probs)
+        fillEmissionMatrix curr_emission_matrix pos_counts pairs rest
 
-calculateEmissions :: String -> String -> [(String,Double)] -> [(String,String)] -> Double
-calculateEmissions wrd cat pos_counts pairs =
-        let 
+calculateEmission :: HMMMatrix -> String -> String -> [(String,Double)] -> [(String,String)] -> HMMMatrix
+calculateEmission Empty _ _ _ _ = Empty
+calculateEmission (Matrix lst1 lst2 lstoflst) wrd cat pos_counts pairs =
+        let
             pairsWithCategory = filter (\x -> snd x == cat) pairs
             catWrdCoocurrence = filter (\x -> fst x == wrd) pairsWithCategory
             catWrdCoocurrenceCount = fromIntegral (length catWrdCoocurrence)
             catCount = snd (head pos_counts)
+            prob = catCount / catWrdCoocurrenceCount
         in
-            catCount / catWrdCoocurrenceCount
+            changeElem_str wrd cat prob (Matrix lst1 lst2 lstoflst)
